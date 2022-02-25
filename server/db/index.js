3c854b21192b68f1cc988b75a0adacd2566b6d74
Kimberley <kimberley@email.com>
2022-02-24 21:39:41 -0500

    feat(server/db/index): Updated model relations.

--- server/db/index.js
+++ server/db/index.js
@@ -7,20 +7,8 @@ const User = require('./models/user')
 const Bookmark = require('./models/bookmark')
 
 /* Create model relations. */
-
-/*
-	A Tag can be assigned to many Bookmarks.
-	A Tag is unique and shared across User's Bookmarks.
-*/
 Tag.belongsToMany(Bookmark, { through: 'bookmark_tag' })
 Bookmark.belongsToMany(Tag, { through: 'bookmark_tag' })
-
-/*
-	A Bookmark can only belong to one User.
-	A User can make many Bookmark.
-*/
-User.belongsToMany(Bookmark, { through: 'user_bookmark' })
-Bookmark.belongsToMany(User, { through: 'user_bookmark' })
 
 /* Include your models in exports. */
 module.exports = {
