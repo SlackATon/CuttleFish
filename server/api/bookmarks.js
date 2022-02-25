8dbd2b6d78aad644cb97f08492652b07d28caa4f
Kimberley <kimberley@email.com>
2022-02-24 23:53:38 -0500

    (fix/feat): Fixed old routes conditional statements. Added two routes.

--- server/api/bookmarks.js
+++ server/api/bookmarks.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const { Bookmark, Tag } = require('../db/index')
 
-/* Get all bookmarks. */
+/* Find all bookmarks. */
 router.get('/', async (req, res, next) => {
 	try {
 		const bookmarks = await Bookmark.findAll({ include: Tag })
@@ -17,10 +17,9 @@ router.post('/', async (req, res, next) => {
 	try {
 		const getBookmark = await Bookmark.findOne({ where: { name: req.body.name } })
 
-		if (getBookmark) {
-			return res.sendStatus(406)
-		} else {
-			const bookmark = await Bookmark.create({ name: req.body.name })
+		if (getBookmark) return res.sendStatus(406)
+		else {
+			await Bookmark.create({ name: req.body.name })
 			return res.sendStatus(201)
 		}
 	} catch (err) {
@@ -28,17 +27,35 @@ router.post('/', async (req, res, next) => {
 	}
 })
 
+/* Find all bookmarks with tags. */
+router.get('/tagged', async (req, res, next) => {
+	try {
+		const bookmarks = await Bookmark.tagged()
+		res.json(bookmarks)
+	} catch (err) {
+		next(err)
+	}
+})
+
+/* Find all bookmarks without tags. */
+router.get('/untagged', async (req, res, next) => {
+	try {
+		const bookmarks = await Bookmark.untagged()
+		res.json(bookmarks)
+	} catch (err) {
+		next(err)
+	}
+})
+
+/* Find by bookmarkId. */
 router.get('/:bookmarkId', async (req, res, next) => {
 	try {
 		const getBookmark = await Bookmark.findByPk(req.params.bookmarkId, {
 			include: Tag
 		})
 
-		if (getBookmark) {
-			return res.send(getBookmark)
-		} else {
-			res.sendStatus(404)
-		}
+		if (getBookmark) return res.send(getBookmark)
+		else res.sendStatus(404)
 	} catch (err) {
 		next(err)
 	}
