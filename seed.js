e2511d3cea3dc59811a2ad3cef61e9e25fda4cb5
Kimberley <kimberley@email.com>
2022-02-26 23:17:44 -0500

    test(seed): Update seed file.

--- seed.js
+++ seed.js
@@ -6,12 +6,31 @@ const seed = async () => {
 	try {
 		await db.sync({ force: true })
 
-		const BOOKMARK_1 = await Bookmark.create({ name: 'https://sequelize.org' })
-		const BOOKMARK_2 = await Bookmark.create({ name: 'https://react-redux.js.org' })
+		const BOOKMARK_1 = await Bookmark.create({
+			title: 'Sequelize',
+			url: 'https://sequelize.org/',
+			description:
+				'Sequelize is a promise-based Node.js ORM for Postgres, MySQL, MariaDB, SQLite and Microsoft SQL Server.'
+		})
+		const BOOKMARK_2 = await Bookmark.create({
+			title: 'React-Redux',
+			url: 'https://react-redux.js.org/',
+			description: 'Official React bindings for Redux'
+		})
+		const BOOKMARK_3 = await Bookmark.create({
+			title: 'YouTube',
+			url: 'https://www.youtube.com/'
+		})
+		const BOOKMARK_4 = await Bookmark.create({
+			url: 'https://www.fullstackacademy.com/',
+			description:
+				'Take the first step toward a thriving career in tech with Fullstack Academy.'
+		})
 
 		const TAG_1 = await Tag.create({ name: 'documentation' })
 		const TAG_2 = await Tag.create({ name: 'sequelize website' })
 		const TAG_3 = await Tag.create({ name: 'react-redux website' })
+		const TAG_4 = await Tag.create({ name: 'grammly' })
 
 		/*                            RELATIONS                              */
 
