e19194f3df28bbc5af50872bcf9c6231140e5aab
Kimberley <kimberley@email.com>
2022-02-28 10:35:48 -0500

    feat: Added a a form to add tags and list tags when clicked on a bookmark.

--- client/components/Bookmark/BookmarkItem.js
+++ client/components/Bookmark/BookmarkItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { TiLink, TiEdit, TiTimes } from 'react-icons/ti'
+import { TiLink, TiEdit, TiTimes, TiPlus } from 'react-icons/ti'
 
-function BookmarkItem(props) {
-	const ellipsis = (str, chars) =>
-		str.length >= chars ? str.slice(0, chars) + '...' : str
+class BookmarkItem extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			edit: false
+		}
+		this.ellipsis = this.ellipsis.bind(this)
+		this.formatDate = this.formatDate.bind(this)
+		this.handleControls = this.handleControls.bind(this)
+		this.handleTags = this.handleTags.bind(this)
+	}
 
-	const formatDate = timestamp => {
+	ellipsis(str, chars) {
+		return str.length >= chars ? str.slice(0, chars) + '...' : str
+	}
+
+	formatDate(timestamp) {
 		const getDate = Date(timestamp).split(' ')
 
 		const month = getDate[1]
@@ -15,41 +27,99 @@ function BookmarkItem(props) {
 		return `${month} ${date} ${year}`
 	}
 
-	console.log('==================>>>>>>>', props.bookmarkData.createdAt)
-	return (
-		<div className="main__bookmark">
-			{/* <div className="main__bookmark-icon">g</div> */}
+	handleControls(type) {
+		const edit = 'edit'
 
-			<div className="main__bookmark-title">
-				{props.bookmarkData.title ? ellipsis(props.bookmarkData.title, 27) : ''}
-			</div>
+		if (type === edit) {
+			this.setState(prvState => ({
+				edit: !prvState.edit
+			}))
+		}
+	}
 
-			<div className="main__bookmark-description">
-				{props.bookmarkData.description
-					? ellipsis(props.bookmarkData.description, 70)
-					: 'no description'}
-			</div>
+	handleTags() {
+		if (this.props.bookmarkData.tags.length > 0) {
+			return (
+				<ul className="main__bookmark-icon-tags-ul">
+					{this.props.bookmarkData.tags.map(tag => (
+						<li className="main__bookmark-icon-tags-li">{tag.name}</li>
+					))}
+				</ul>
+			)
+		}
+	}
 
-			<div className="main__bookmark-date">
-				{formatDate(props.bookmarkData.createdAt)}
-			</div>
+	render() {
+		console.log('.......', this.props.bookmarkData)
+		return (
+			<div className="main__bookmark">
+				{/* <div className="main__bookmark-icon">g</div> */}
 
-			<div className="main__bookmark-icon-wrapper">
-				<a
-					target="_blank"
-					href={props.bookmarkData.url}
-					className="main__bookmark-icon">
-					<TiLink />
-				</a>
-				<div href="" className="main__bookmark-icon">
-					<TiEdit />
+				<div className="main__bookmark-title">
+					{this.props.bookmarkData.title
+						? this.ellipsis(this.props.bookmarkData.title, 27)
+						: ''}
 				</div>
-				<div href="" className="main__bookmark-icon">
-					<TiTimes />
+
+				<div className="main__bookmark-description">
+					{this.props.bookmarkData.description
+						? this.ellipsis(this.props.bookmarkData.description, 70)
+						: 'no description'}
+				</div>
+
+				<div className="main__bookmark-date">
+					{this.formatDate(this.props.bookmarkData.createdAt)}
+				</div>
+
+				<div className="main__bookmark-controls-wrapper">
+					<div className="main__bookmark-icon-wrapper">
+						<a
+							target="_blank"
+							href={this.props.bookmarkData.url}
+							className="main__bookmark-icon">
+							<TiLink />
+						</a>
+
+						{/* <div className="main__bookmark-icon-tags main__bookmark-icon-hidden"></div> */}
+					</div>
+
+					<div className="main__bookmark-icon-wrapper">
+						<div
+							className="main__bookmark-icon"
+							onClick={() => this.handleControls('edit')}>
+							<TiEdit />
+						</div>
+
+						{this.state.edit ? (
+							<div className="main__bookmark-icon-tags main__bookmark-icon-hidden">
+								{this.handleTags()}
+								<form className="main__bookmark-icon-tags-form">
+									<input
+										className="main__bookmark-icon-tags-text"
+										type="text"
+									/>
+									<button
+										type="submit"
+										className="main__bookmark-icon-tags-btn">
+										<TiPlus />
+									</button>
+								</form>
+							</div>
+						) : (
+							' '
+						)}
+					</div>
+
+					<div className="main__bookmark-icon-wrapper">
+						<div className="main__bookmark-icon">
+							<TiTimes />
+						</div>
+						{/* <div className="main__bookmark-icon-tags main__bookmark-icon-hidden"></div> */}
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default BookmarkItem
