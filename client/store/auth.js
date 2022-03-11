7fb7bbf658a7f6319a81953db874139e07e7b7b7
Kimberley <kimberley@email.com>
2022-03-11 17:02:05 -0500

    fix(client/store): Remove alert message when user toggles between signin and signup.

--- client/store/auth.js
+++ client/store/auth.js
@@ -4,11 +4,13 @@ const MANUAL_SIGNIN = 'MANUAL_SIGNIN'
 const MANUAL_SIGNIN_ERROR = 'MANUAL_SIGNIN_ERROR'
 const SIGN_UP = 'SIGN_UP'
 const SIGN_UP_ERROR = 'SIGN_UP_ERROR'
+const CLEAR_ALERT = 'CLEAR_ALERT'
 
 const _manualSignin = token => ({ type: MANUAL_SIGNIN, token })
 const _manualSigninError = alert => ({ type: MANUAL_SIGNIN_ERROR, alert })
 const _signUp = token => ({ type: SIGN_UP, token })
 const _signUpError = alert => ({ type: SIGN_UP_ERROR, alert })
+export const _clearAlert = () => ({ type: CLEAR_ALERT })
 
 export const manualSignin = (formObj, history) => {
 	return async dispatch => {
@@ -27,6 +29,7 @@ export const manualSignin = (formObj, history) => {
 			if (token) {
 				action = _manualSignin(token)
 				localStorage.setItem('token', token)
+				history.push('/') /* Redirects to main page. */
 			} else {
 				localStorage.clear('token')
 				action = _manualSigninError(alert)
@@ -57,11 +60,11 @@ export const signup = (formObj, history) => {
 			if (token) {
 				action = _signUp(token)
 				localStorage.setItem('token', token)
+				history.push('/') /* Redirects to main page. */
 			} else {
 				localStorage.clear('token')
 				action = _signUpError(alert)
 			}
-
 			dispatch(action)
 		} catch (err) {
 			console.error(err)
@@ -81,6 +84,8 @@ const authReducer = (state = init, action) => {
 			return { ...state, alert: '', token: action.token }
 		case SIGN_UP_ERROR:
 			return { ...state, token: '', alert: action.alert }
+		case CLEAR_ALERT:
+			return { ...state, alert: '' }
 		default:
 			return state
 	}
