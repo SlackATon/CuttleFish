45cab4301f2126828496c19a7d330a2246b40e74
Kimberley <kimberley@email.com>
2022-03-12 18:09:25 -0500

    feat(client/store): Edited helper store.

--- client/store/helper.js
+++ client/store/helper.js
@@ -2,10 +2,25 @@
 
 const ellipsis = (str, chars) => (str.length >= chars ? str.slice(0, chars) + '...' : str)
 
+const dateFormatter = timestamp => {
+	const getDate = Date(timestamp).split(' ')
+
+	const month = getDate[1]
+	const date = getDate[2]
+	const year = getDate[3]
+
+	return `${month} ${date} ${year}`
+}
+
 const GET_ELLIPSIS = 'GET_ELLIPSIS'
+const GET_DATE_FORMATTER = 'GET_DATE_FORMATTER'
 
 export const _getEllipsis = () => {
 	return { type: GET_ELLIPSIS, fn: ellipsis, name: 'ellipsis' }
+}
+
+export const _getDateFormatter = () => {
+	return { type: GET_DATE_FORMATTER, fn: dateFormatter, name: 'dateFormatter' }
 }
 
 const init = {}
@@ -13,7 +28,9 @@ const init = {}
 const helperReducer = (state = init, action) => {
 	switch (action.type) {
 		case GET_ELLIPSIS:
-			return { [action.name]: action.fn }
+			return { ...state, [action.name]: action.fn }
+		case GET_DATE_FORMATTER:
+			return { ...state, [action.name]: action.fn }
 		default:
 			return state
 	}
