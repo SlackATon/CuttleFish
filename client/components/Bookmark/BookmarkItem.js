755dde73cb591065b4a54bc738ee094a3571775f
Kimberley <kimberley@email.com>
2022-03-12 18:10:16 -0500

    feat(client/components): Displays each bookmark item.

--- client/components/Bookmark/BookmarkItem.js
+++ client/components/Bookmark/BookmarkItem.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { TiLink, TiEdit, TiTimes, TiPlus } from 'react-icons/ti'
 import { HiHashtag } from 'react-icons/hi'
+
+import { _getEllipsis, _getDateFormatter } from './../../store/helper'
 
 class BookmarkItem extends React.Component {
 	constructor() {
@@ -8,42 +11,31 @@ class BookmarkItem extends React.Component {
 		this.state = {
 			edit: false
 		}
-		this.ellipsis = this.ellipsis.bind(this)
-		this.formatDate = this.formatDate.bind(this)
+
 		this.handleControls = this.handleControls.bind(this)
 		this.handleCurrentTags = this.handleCurrentTags.bind(this)
 		this.handleAddedTags = this.handleAddedTags.bind(this)
 	}
 
-	ellipsis(str, chars) {
-		return str.length >= chars ? str.slice(0, chars) + '...' : str
-	}
-
-	formatDate(timestamp) {
-		const getDate = Date(timestamp).split(' ')
-
-		const month = getDate[1]
-		const date = getDate[2]
-		const year = getDate[3]
-
-		return `${month} ${date} ${year}`
+	componentDidMount() {
+		this.props.getEllipsis()
+		this.props.getDataFormatter()
 	}
 
 	handleControls(type) {
-		const edit = 'edit'
-
-		if (type === edit) {
+		if (type === 'edit') {
 			this.setState(prvState => ({
 				edit: !prvState.edit
 			}))
 		}
 	}
 
+	/* Will display the current tags for that bookmark. */
 	handleCurrentTags() {
 		return (
 			<ul className="main__bookmark-icon-tags-ul">
 				{this.props.bookmarkData.tags.map(tag => (
-					<li className="main__bookmark-icon-tags-li">
+					<li key={tag.id} className="main__bookmark-icon-tags-li">
 						<div className="main__bookmark-icon-tags-li-hastag">
 							<HiHashtag />
 						</div>
@@ -54,26 +46,106 @@ class BookmarkItem extends React.Component {
 		)
 	}
 
+	/* Will display previos tags as suggestions. */
 	handleAddedTags() {
 		return (
 			<div>
 				<ul className="main__bookmark-icon-tags-ul main__bookmark-icon-search-tags-ul">
-					{this.props.bookmarkData.tags.map(tag => (
-						<li className="main__bookmark-icon-tags-li">
-							<div className="main__bookmark-icon-tags-li-hastag">
-								<HiHashtag />
-							</div>
-							git-resource
-						</li>
-					))}
+					<li className="main__bookmark-icon-tags-li">
+						<div className="main__bookmark-icon-tags-li-hastag">
+							<HiHashtag />
+						</div>
+						git-resource
+					</li>
 				</ul>
 			</div>
 		)
 	}
 
 	render() {
-		return <div className="main__bookmark">Title</div>
+		console.log('here', this.props.ellipsis)
+		console.log('and here', this.props.dateFormatter)
+
+		return (
+			<div className="main__bookmark">
+				<div className="main__bookmark-title">
+					{this.props.bookmarkData.title && this.props.ellipsis
+						? this.props.ellipsis(this.props.bookmarkData.title, 27)
+						: ''}
+				</div>
+
+				<div className="main__bookmark-description">
+					{this.props.bookmarkData.description && this.props.ellipsis
+						? this.props.ellipsis(this.props.bookmarkData.description, 70)
+						: 'no description'}
+				</div>
+
+				<div className="main__bookmark-date">
+					{this.props.bookmarkData.createdAt && this.props.dateFormatter
+						? this.props.dateFormatter(this.props.bookmarkData.createdAt)
+						: null}
+				</div>
+
+				<div className="main__bookmark-controls-wrapper">
+					<div className="main__bookmark-icon-wrapper">
+						<a
+							target="_blank"
+							href={this.props.bookmarkData.url}
+							className="main__bookmark-icon">
+							<TiLink />
+						</a>
+					</div>
+
+					<div className="main__bookmark-icon-wrapper">
+						<div
+							className="main__bookmark-icon"
+							onClick={() => this.handleControls('edit')}>
+							<TiEdit />
+						</div>
+
+						{this.state.edit ? (
+							<div className="main__bookmark-icon-tags main__bookmark-icon-hidden">
+								{this.handleCurrentTags()}
+								<form className="main__bookmark-icon-tags-form">
+									<input
+										className="main__bookmark-icon-tags-text"
+										type="text"
+									/>
+									<button
+										type="submit"
+										className="main__bookmark-icon-tags-btn">
+										<TiPlus />
+									</button>
+								</form>
+
+								{this.handleAddedTags()}
+							</div>
+						) : null}
+					</div>
+
+					<div className="main__bookmark-icon-wrapper">
+						<div className="main__bookmark-icon">
+							<TiTimes />
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 	}
 }
 
-export default BookmarkItem
+const mapStateToProps = state => {
+	return {
+		ellipsis: state.helper.ellipsis,
+		dateFormatter: state.helper.dateFormatter
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		getEllipsis: () => dispatch(_getEllipsis()),
+		getDataFormatter: () => dispatch(_getDateFormatter())
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookmarkItem)
