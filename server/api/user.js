e18c0249cdb96ced990aeaffe54ef90cb3040849
Kimberley <kimberley@email.com>
2022-03-13 19:33:43 -0400

    feat(server/api): Router so users can change theme.

--- server/api/user.js
+++ server/api/user.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 const { User } = require('../db/index')
 
 /* Find a users username and icon. */
+/* /api/user */
 router.get('/', async (req, res, next) => {
 	try {
 		const token = req.headers.authorization
@@ -19,6 +20,37 @@ router.get('/', async (req, res, next) => {
 				})
 
 				return res.send(user)
+			}
+		}
+
+		return res.sendStatus(404)
+	} catch (err) {
+		next(err)
+	}
+})
+
+/* Route so users can change their theme */
+/* /api/user/theme */
+router.post('/theme', async (req, res, next) => {
+	try {
+		const token = req.headers.authorization
+		const theme = req.body.theme
+
+		/* If the headers include 'authorization' */
+		if (token) {
+			const match = await User.decryptToken(token)
+
+			/* If the decrypted token matches a user in the database. */
+			if (match) {
+				const user = await User.findOne({
+					where: { email: match.email }
+				})
+
+				if (user) {
+					await user.update({ theme: theme })
+
+					res.json(user.theme)
+				}
 			}
 		}
 
