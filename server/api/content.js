efae0fa568764f244ebbfbf11e74bcdf073f4542
Kimberley <kimberley@email.com>
2022-03-13 00:46:11 -0500

    feat(server/api): Added route to add bookmarks.

--- server/api/content.js
+++ server/api/content.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 const { User, Bookmark, Tag } = require('../db/index')
 
@@ -73,6 +75,50 @@ router.get('/user/tags', async (req, res, next) => {
 				})
 
 				return res.json(userBookmarks)
+			}
+		}
+
+		res.sendStatus(404)
+	} catch (err) {
+		next(err)
+	}
+})
+
+/* Add a bookmark */
+router.post('/addbookmark', async (req, res, next) => {
+	try {
+		const token = req.headers.authorization
+		let url = req.body.url
+
+		/* If the token exist in local storage. */
+		if (token) {
+			const decrypted = await User.decryptToken(token)
+
+			/*
+				If the decrypted token matches the on in the database.
+			*/
+			if (decrypted) {
+				const user = await User.findOne({ where: { email: decrypted.email } })
+				url.endsWith('/') ? (url = url.slice(url, url.length - 1)) : null
+
+				/* Make a request to the URL and stores it 'data' for 'cherrio' to use. */
+				const { data } = await axios.get(req.body.url)
+
+				/* If 'data' has a value. */
+				if (data) {
+					/* 'cheerio' does its thing. */
+					const $ = cheerio.load(data)
+					const title = $('title').html()
+					const description = $("meta[name='description']").attr('content')
+
+					await user.createBookmark({
+						url: url,
+						title: title || '',
+						description: description || ''
+					})
+
+					return res.sendStatus(201)
+				}
 			}
 		}
 
