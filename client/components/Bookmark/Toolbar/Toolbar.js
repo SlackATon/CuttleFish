e8f9c0493a6bebf833adbf41c1661be2172e4c46
Kimberley <kimberley@email.com>
2022-03-13 05:01:08 -0400

    feat: Added a Toolbar for when users are on the /bookmarks page.

--- client/components/Bookmark/Toolbar/Toolbar.js
+++ client/components/Bookmark/Toolbar/Toolbar.js
@@ -1,23 +1,32 @@
 import React from 'react'
+import { AiOutlineLink } from 'react-icons/ai'
 
 function Toolbar() {
 	return (
 		<div className="main__top-bar">
-			<div className="main__top-bar-icon">🍬</div>
-			<h3 className="main__top-bar-name">News & Social</h3>
+			<div className="main__top-bar-left">
+				{/* <div className="main__top-bar-icon">🍬</div> */}
+				<h3 className="main__top-bar-name">News & Social</h3>
+			</div>
 
-			<div className="main__top-bar-advance-controls">
-				<button className="main__top-bar-btn main__top-bar-btn-advance">
-					...
-				</button>
+			<div className="main__top-bar-right">
+				<form className="main__topbar-right-input-form">
+					<div className="main__topbar-right-input-form-icon">
+						<AiOutlineLink />
+					</div>
 
-				<div className="main__top-bar-advance-inner-controls main__top-bar-advance-inner-controls--hidden">
-					<button className="main__top-bar-advance-inner-controls-btn">
-						Delete all
+					<input
+						className="main__topbar-right-input"
+						placeholder="Enter URL"
+						type="text"
+					/>
+					<button type="submit" className="main__top-bar-submit">
+						Add
 					</button>
-					<button className="main__top-bar-advance-inner-controls-btn">
-						Untag all
-					</button>
+				</form>
+				<div className="main__top-bar-button-wrapper">
+					<button className="main__top-bar-button">Untag All</button>
+					<button className="main__top-bar-button">Delete All</button>
 				</div>
 			</div>
 		</div>
