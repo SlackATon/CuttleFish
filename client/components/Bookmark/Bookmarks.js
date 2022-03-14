1b07da4b5404090009d1e81a3586646e1639327e
Kimberley <kimberley@email.com>
2022-03-13 20:50:59 -0400

    feat(client/components): Added function to get user theme when conponent mounts.

--- client/components/Bookmark/Bookmarks.js
+++ client/components/Bookmark/Bookmarks.js
@@ -6,6 +6,7 @@ import Toolbar from './Toolbar/Toolbar'
 import BookmarkContainer from './BookmarkContainer'
 
 import { checkToken } from '../../store/auth'
+import { getTheme } from '../../store/app'
 
 class Bookmarks extends React.Component {
 	constructor() {
@@ -14,10 +15,32 @@ class Bookmarks extends React.Component {
 
 	componentDidMount() {
 		this.props.checkToken()
+		this.props.getTheme()
 	}
 
 	componentDidUpdate() {
 		this.props.checkToken()
+
+		//! TOFIX: IDEK
+		if (this.props.theme === 'Light') {
+			document.documentElement.style.setProperty('--clr-dark', '#111111')
+			document.documentElement.style.setProperty('--clr-dark-accent', '#5f5f5f')
+			document.documentElement.style.setProperty('--clr-dark-light', '#1c1c1c')
+			document.documentElement.style.setProperty('--clr-light', '#fdfdfd')
+			document.documentElement.style.setProperty('--clr-light-dark', '#dedede')
+		} else if (this.props.theme === 'Dark') {
+			document.documentElement.style.setProperty('--clr-dark', '#0a0a0a')
+			document.documentElement.style.setProperty('--clr-dark-accent', '#323232')
+			document.documentElement.style.setProperty('--clr-dark-light', '#111111')
+			document.documentElement.style.setProperty('--clr-light', '#9c9c9c')
+			document.documentElement.style.setProperty('--clr-light-dark', '#636363')
+		} else if (this.props.theme === 'Forest') {
+			document.documentElement.style.setProperty('--clr-dark', '#02361d')
+			document.documentElement.style.setProperty('--clr-dark-accent', '#0e6144')
+			document.documentElement.style.setProperty('--clr-dark-light', '#0b3b2a')
+			document.documentElement.style.setProperty('--clr-light', '#d7f5ee')
+			document.documentElement.style.setProperty('--clr-light-dark', '#abd4c4')
+		}
 	}
 
 	render() {
@@ -33,9 +56,16 @@ class Bookmarks extends React.Component {
 	}
 }
 
-const mapDispatchToProps = (dispatch, { history }) => {
+const mapStateToProps = state => {
 	return {
-		checkToken: () => dispatch(checkToken(history))
+		theme: state.app.theme
 	}
 }
-export default connect(null, mapDispatchToProps)(Bookmarks)
+
+const mapDispatchToProps = (dispatch, { history }) => {
+	return {
+		checkToken: () => dispatch(checkToken(history)),
+		getTheme: () => dispatch(getTheme())
+	}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Bookmarks)
